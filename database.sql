71031c818e9fd9be2ff62857a910dd03b37125e8
naingkhanthtet <hoceice420@gmail.com>
2023-10-09 23:06:18 +0630

    updated Menu table, User table, Order table!

--- database.sql
+++ database.sql
@@ -10,17 +10,30 @@ grant all privileges on canteen.* to 'canteen'@'localhost';
 
 use canteen;
 
-create table User (
-    id INT AUTO_INCREMENT primary key,
+create table Users (
+    uid INT AUTO_INCREMENT primary key,
     email varchar(50) unique not null,
-    passw varchar(15) unique not null,
+    passwd varchar(15) unique not null,
     phone varchar(11) unique not null,
-    role ENUM('Client', 'Admin')  
+    batch INT not null,
+    urole ENUM('Client', 'Admin')  
 );
 
-create table foods (
-    fid INT AUTO_INCREMENT primary key,
-    food varchar(255) unique not null,
+create table Menus (
+    mid INT AUTO_INCREMENT primary key,
+    mname varchar(255) unique not null,
+    quantity INT not null,
     price INT not null,
-    role ENUM('Curry', 'Drink', 'Fries', 'Salad', 'Fast', 'Soup')
+    mrole ENUM('Curry', 'Drink', 'Fries', 'Salad', 'Fast', 'Soup')
 );
+
+insert into Users(email, passwd, phone, batch, urole) values ('naing@gmail.com', 'Naing15!', '09123456789', 10, 'Client');
+
+create table Orders (
+    oid INT AUTO_INCREMENT primary key,
+    uid INT not null,
+    odate DATETIME not null, 
+    mid INT not null,
+    quantity INT not null,
+    price INT not null
+)
