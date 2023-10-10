5b822cde0d0d917e4f4f654c63567f8fa54fe347
naingkhanthtet <hoceice420@gmail.com>
2023-10-10 13:56:05 +0630

    added register form html file (test purpose only)

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get('/', (req, res) => {
     res.render("index")
 });
 
+app.get('/register', (req, res) => {
+    res.render("register")
+});
+
 app.listen(8000, () => {
     console.log('Server started on port 8000');
 });
