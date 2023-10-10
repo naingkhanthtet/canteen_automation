302a50016281ae7a4ff15da8471b2e174c846b02
naingkhanthtet <hoceice420@gmail.com>
2023-10-10 22:02:49 +0630

    idk, I updated app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var express = require('express');
-var mysql = require('mysql');
-var path = require('path');
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
 const app = express();
-const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const async = require('hbs/lib/async');
+const { log } = require('console');
 
-dotenv.config({ path: './.env'})
+require('dotenv').config({ path: './.env'})
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
 });
 
 const publicDir = path.join(__dirname, './public');
@@ -18,10 +21,46 @@ app.use(express.static(publicDir));
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
 
+app.post('/createUser', async (req, res) => {
+    const { email, passwd, phone, batch, urole } = req.body;
+    const hashedPassword = await bcrypt.hash(passwd, 10);
+
+    db.getConnection(async (err, connection) => {
+        if (err) throw err;
+
+        const sqlSearch = "select * from Users WHERE email = ?";
+        const search_query = mysql.format(sqlSearch,[email]);
+
+        const sqlInsert = "insert into Users values (0,?,?,?,?,?)";
+        const insert_query = mysql.format(sqlInsert,[email, hashedPassword, phone, batch, urole]);
+
+        await connection.query (search_query, async (err, result) => {
+            if (err) throw err;
+            console.log("-----> Search results");
+            console.log(result.length);
+
+            if (result.length != 0) {
+                connection.release();
+                console.log("-----> user already exists");
+                res.sendStatus(409);
+            } else {
+                await connection.query (insert_query, (err, result) => {
+                    connection.release();
+
+                    if (err) throw err;
+                    console.log("-----> created new user");
+                    console.log(result.insertId);
+                    res.sendStatus(201);
+                })
+            }
+        });
+    });
+});
+/*
 // https://www.npmjs.com/package/hbs
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
-
+*/
 
 db.connect(function(err) {
     if (err) throw err;
@@ -31,6 +70,7 @@ db.connect(function(err) {
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
-app.listen(8000, () => {
-    console.log('Server started on port 8000');
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}...`);
 });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
+const mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
 
 exports.register = (req, res) => {
-    console.log(req.body);
     res.send("form submitted");
 }
